21e905bde93cf97b0fa368a1bc42c376eb6bf1a6
IsaacIngram22 <iingram@students.casciahall.com>
2022-01-18 13:35:42 -0600

    Initial commit

---
+++ RapidReactOfficial/src/main/java/frc/robot/Projectile/ProjectileConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.Projectile;
+
+public class ProjectileConstants {
+
+    public static final double gravity = -9.8; // The acceleration due to gravity in meters per second squared
+    public static final double kDragCoefficient = 0.23; // The coefficient of drag acting on the ball
+    public static final double kGoalHeightMeters = 2.64; // The height of the rim of the target in meters.
+    public static final double kRobotHeightInMeters = 1; // The height of the robot in meters
+
+}
---
+++ RapidReactOfficial/src/main/java/frc/robot/Projectile/Vector.java
@@ -0,0 +1,14 @@
+package frc.robot.Projectile;
+
+public class Vector {
+    
+    double velocity;
+    double angle;
+
+    public Vector(double velocity, double angle) {
+        this.velocity = velocity;
+        this.angle = angle;
+    }
+
+}
+
