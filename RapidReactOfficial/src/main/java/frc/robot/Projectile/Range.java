2b18a938d4e8d0b624f3de9f6a24933bf0cf435a
IsaacIngram <iingram@students.casciahall.com>
2022-02-10 12:38:44 -0600

    Fix compare issue
    This class needs to be comparable so that an instance of it can be compared with other instances in a treemap. This commit adds that compare features.

--- RapidReactOfficial/src/main/java/frc/robot/Projectile/Range.java
+++ RapidReactOfficial/src/main/java/frc/robot/Projectile/Range.java
@@ -1,6 +1,8 @@
 package frc.robot.Projectile;
 
-public class Range {
+public class Range implements Comparable<Range>{
+
+    int id;
 
     public double minValue;
     public double maxValue;
@@ -13,6 +15,9 @@ public class Range {
     public Range(double minValue, double maxValue) {
         this.minValue = minValue;
         this.maxValue = maxValue;
+        // Convert min value to an int and set it as the ID.
+        // This is necessary for this object to be comparable.
+        id = Math.round((float)minValue);
     }
 
     /**
@@ -24,6 +29,11 @@ public class Range {
      */
     public boolean isValueInRange(double valueToCheck) {
         return valueToCheck >= minValue && valueToCheck < maxValue;
+    }
+
+    @Override
+    public int compareTo(Range object) {
+        return id-object.id;
     }
     
 }
