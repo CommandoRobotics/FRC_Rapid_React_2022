05afbc4f403c3fbfce9c68e31143722ef6abed69
IsaacIngram <iingram@students.casciahall.com>
2022-02-17 18:08:10 -0600

    update ports
    Update ports to prevent port interference so that the code can actual build

--- RapidReactOfficial/src/main/java/frc/robot/Constants/ConstantsPorts.java
+++ RapidReactOfficial/src/main/java/frc/robot/Constants/ConstantsPorts.java
@@ -14,15 +14,20 @@ public class ConstantsPorts {
      * SHOOTER
      */
     public static final int bottomLeaderPort = 18;
-    public static final int bottomFollowerOnePort = 6;
-    public static final int bottomFollowerTwoPort = 7;
-    public static final int topPort = 8;
-    public static final int kickwheelPort = 9;
+    public static final int bottomFollowerOnePort = 19;
+    public static final int bottomFollowerTwoPort = 20;
+    public static final int topPort = 21;
+    public static final int kickwheelPort = 22;
 
+    /**
+     * Climber
+     */
     public static final int leftWinchId = 10;
     public static final int rightWinchId = 11;
     public static final int midClimberId = 12;
     public static final int traversalClimberId = 13;
+    public static final int topLimitSwitchPort = 0;
+    public static final int bottomLimitSwitchPort = 1;
 
     /**
      * Intake
@@ -33,18 +38,18 @@ public class ConstantsPorts {
     /**
      * Index
      */
-    public static final int horizontalId = 0;
-    public static final int verticalId = 0;
-    public static final int transferLeaderId = 0;
-    public static final int transferFollowerId = 0;
+    public static final int horizontalId = 6;
+    public static final int verticalId = 7;
+    public static final int transferLeaderId = 8;
+    public static final int transferFollowerId = 9;
 
     /**
      * AUTO AIM
      */
     public static final int panId = 16;
     public static final int tiltId = 17;
-    public static final int panLimitSwitchPort = 0;
-    public static final int tiltLimitSwitchPort = 1;
+    public static final int panLimitSwitchPort = 2;
+    public static final int tiltLimitSwitchPort = 3;
 
 
 }
