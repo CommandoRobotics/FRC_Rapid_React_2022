6fe73ff088700ee4f944799b666f0bee6673a251
IsaacIngram <iingram@students.casciahall.com>
2022-03-10 07:30:31 -0600

    Update auton
    Update the ideal auton to be better with new paths and commands and sensor based features

--- RapidReactOfficial/src/main/java/frc/robot/commands/AutonomousCommands/IdealAutonomous.java
+++ RapidReactOfficial/src/main/java/frc/robot/commands/AutonomousCommands/IdealAutonomous.java
@@ -5,11 +5,15 @@
 package frc.robot.commands.AutonomousCommands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.AutoAimCommands.AutoAimAutonomousCommand;
+import frc.robot.commands.IndexCommands.RunIndexToShootAutonomousCommand;
 import frc.robot.commands.IndexCommands.RunIndexToShootCommand;
+import frc.robot.commands.IntakeCommands.IntakeCommand;
+import frc.robot.commands.ShooterCommands.RevShooterAtAutoVelocityAutonomousCommand;
 import frc.robot.commands.ShooterCommands.SetShooterToVelocityCommand;
 import frc.robot.subsystems.AutoAimSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
@@ -32,95 +36,120 @@ public class IdealAutonomous extends SequentialCommandGroup {
     AutoAimSubsystem autoAimSubsystem
     ) 
     {
-
     addCommands(
-
-    // Aim towards the hub
-    new AutoAimAutonomousCommand(driveSubsystem, autoAimSubsystem).andThen(new PrintCommand("Finished AutoAiming Towards Hub"))
-    // Set the shooter velocity
-    .alongWith(new SetShooterToVelocityCommand(2000, shooterSubsystem).andThen(new PrintCommand("Finished setting shooter to 2000 RPM")))
-    // Run the index to shoot
-    .andThen(new RunIndexToShootCommand(indexSubsystem).andThen(new PrintCommand("Finished starting the index to shoot"))),
-    // Wait to give the robot time to shoot
-    new WaitCommand(3).andThen(new PrintCommand("Finished waiting 3 seconds")),
-
-    // Stop index and shooter
-    new InstantCommand(indexSubsystem::stopAll, indexSubsystem),
-    new InstantCommand(shooterSubsystem::stop, shooterSubsystem).andThen(new PrintCommand("Finished stopping index and shooter")),
-
-    // Extend intake
-    new InstantCommand(intakeSubsystem::extend, intakeSubsystem).andThen(new PrintCommand("Finished extending intake")),
-
-    // Start intake
-    new InstantCommand(intakeSubsystem::intakeIn, intakeSubsystem).andThen(new PrintCommand("Finished starting intake"))
-    // Drive to the first ball
-    .alongWith(
-      driveSubsystem.newCommandFromTrajectory(
+      // Extend the intake, start the intake, and drive at the same time
+      new ParallelCommandGroup(
+        // Extend the intake and THEN start it
+        new SequentialCommandGroup(
+          // Extend the intake
+          new InstantCommand(intakeSubsystem::extend, intakeSubsystem).andThen(
+            new PrintCommand("Finished extending Intake")),
+          // Start the intake
+          new IntakeCommand(intakeSubsystem, indexSubsystem).andThen(
+            new PrintCommand("Finished starting intake"))
+        ),
+        // Drive to the ball outside tarmac
+        driveSubsystem.newCommandFromTrajectory(
         PathFetcher.fetchIdeal(0), 
-        true, // This is the initial pose
-        false // The robot should not stop once this path is complete
-        ).andThen(new PrintCommand("Finished Ideal Path 1"))
-    ),
+        true, // This is the intial pose
+        true // The robot should stop after this trajectory is finished
+        ).andThen(
+          new PrintCommand("Finished Ideal Path 1"))
+      ),
+      // Stop the index and intake
+      new InstantCommand(indexSubsystem::stopAll),
+      new InstantCommand(intakeSubsystem::stop),
 
-    // Drive to the second ball
-    driveSubsystem.newCommandFromTrajectory(
-      PathFetcher.fetchIdeal(1), 
-      false, // This is not the initial pose
-      true // The robot should stop at the end of this path
-      ).andThen(new PrintCommand("Finished Ideal Path 2")),
+      // Rev the shooter to the automatically determined velocity
+      new RevShooterAtAutoVelocityAutonomousCommand(shooterSubsystem)
+      .andThen(new PrintCommand("Finished reving shooter")),
 
-    // Stop intake
-    new InstantCommand(intakeSubsystem::stop, intakeSubsystem).andThen(new PrintCommand("Finished stopping intake")),
+      // Shoot two balls
+      new RunIndexToShootAutonomousCommand(indexSubsystem)
+      .andThen(new PrintCommand("Finished running index to shoot 2 balls")),
+
+      // Stop shooter and index
+      new InstantCommand(shooterSubsystem::stop)
+      .andThen(new InstantCommand(indexSubsystem::stopAll))
+      .andThen(new PrintCommand("Finished stopping the shooter and the index")),
+
+      // Start driving to the next ball while running the intake
+      new IntakeCommand(intakeSubsystem, indexSubsystem)
+      .alongWith(
+        driveSubsystem.newCommandFromTrajectory(
+          PathFetcher.fetchIdeal(1),
+          false,
+          true
+        )
+      ),
+      new PrintCommand("Finished Ideal Path 2"),
+
+      // Stop the index and intake
+      new InstantCommand(indexSubsystem::stopAll),
+      new InstantCommand(intakeSubsystem::stop),
+      new PrintCommand("Finished stopping index and intake"),
+
+      // Rev the shooter ot the automatically determined velocity
+      new RevShooterAtAutoVelocityAutonomousCommand(shooterSubsystem),
+      new PrintCommand("Finished reving shooter to auto velocity"),
+
+      // Run index to shoot one ball
+      new RunIndexToShootAutonomousCommand(1, indexSubsystem),
+      new PrintCommand("Finished shooting 1 ball"),
+
+
+      // Stop shooter and index
+      new InstantCommand(shooterSubsystem::stop)
+      .andThen(new InstantCommand(indexSubsystem::stopAll))
+      .andThen(new PrintCommand("Finished stopping the shooter and the index")),
+
+      // Starting driving to the next ball while running the intake
+      // Start driving to the next ball while running the intake
+      new IntakeCommand(intakeSubsystem, indexSubsystem)
+      .alongWith(
+        driveSubsystem.newCommandFromTrajectory(
+          PathFetcher.fetchIdeal(2),
+          false,
+          true
+        )
+      ),
+      new PrintCommand("Finished Ideal Path 3"),
+
+      // Retract the intake for protection.
+      // Note: we keep running it here to make sure we fully intake the balls we gathered
+      new InstantCommand(intakeSubsystem::retract),
+      new PrintCommand("Finished retracting intake"),
+
+      // Drive to our shooting position
+      driveSubsystem.newCommandFromTrajectory(
+          PathFetcher.fetchIdeal(3),
+          false,
+          true
+      ),
+      new PrintCommand("Finished Ideal Path 4"),
+
+      // Stop intake and index
+      new InstantCommand(indexSubsystem::stopAll),
+      new InstantCommand(intakeSubsystem::stop),
+      new PrintCommand("Finished stopping index and intake"),
+
+      // Rev shooter to automatically determined velocity
+      new RevShooterAtAutoVelocityAutonomousCommand(shooterSubsystem),
+      new PrintCommand("Finished reving shooter to auto velocity"),
+
+      // Shoot two balls
+      new RunIndexToShootAutonomousCommand(indexSubsystem),
+      new PrintCommand("Finished shooting 2 balls"),
     
-    // Aim towards the hub
-    new AutoAimAutonomousCommand(driveSubsystem, autoAimSubsystem).andThen(new PrintCommand("Finished AutoAiming towards hub"))
-    // Set the shooter velocity
-    .alongWith(new SetShooterToVelocityCommand(3000, shooterSubsystem).andThen(new PrintCommand("Finished setting shooter to 3000 rpm")))
-    // Run the index to shoot
-    .andThen(new RunIndexToShootCommand(indexSubsystem).andThen(new PrintCommand("Finished starting index to shoot"))),
-    // Wait to give the robot time to shoot
-    new WaitCommand(3).andThen(new PrintCommand("Finished waiting 3 seconds")),
 
-    // Stop index and shooter
-    new InstantCommand(indexSubsystem::stopAll, indexSubsystem),
-    new InstantCommand(shooterSubsystem::stop, shooterSubsystem).andThen(new PrintCommand("Finished stopping index and shooter")),
-    
-    // Start intake
-    new InstantCommand(intakeSubsystem::intakeIn, intakeSubsystem).andThen(new PrintCommand("Finished starting intake")),
+      // Stop everything
+      new InstantCommand(intakeSubsystem::stop),
+      new InstantCommand(indexSubsystem::stopAll),
+      new InstantCommand(shooterSubsystem::stop),
+      new PrintCommand("Finished stopping all motors"),
 
-    // Drive to two balls by terminal
-    driveSubsystem.newCommandFromTrajectory(
-      PathFetcher.fetchIdeal(2), 
-      false, // This is not the initial pose
-      false // The robot should not stop after this path
-    ).andThen(new PrintCommand("Finished Ideal Path 3")),
+      new PrintCommand("Finished IdealAuto")
 
-    // Stop the intake
-    new InstantCommand(intakeSubsystem::stop, intakeSubsystem).andThen(new PrintCommand("Finished stopping the intake")),
-
-    // Drive to better shooting position
-    driveSubsystem.newCommandFromTrajectory(
-      PathFetcher.fetchIdeal(3),
-      false, // This is not the initial pose
-      true // The robot should stop after this path
-    ).andThen(new PrintCommand("Finished Ideal Path 4")),
-
-    // Aim towards the hub
-    new AutoAimAutonomousCommand(driveSubsystem, autoAimSubsystem).andThen(new PrintCommand("Finished AutoAiming"))
-    // Rev shooter to velocity
-    .alongWith(new SetShooterToVelocityCommand(3500, shooterSubsystem).andThen(new PrintCommand("Finished setting Shooter to 3500 RPM")))
-    // Run index to shoot
-    .andThen(new RunIndexToShootCommand(indexSubsystem).andThen(new PrintCommand("Finished starting the index to shoot"))),
-    // Wait a few seconds to shoot
-    new WaitCommand(3).andThen(new PrintCommand("Finished waiting 3 seconds")),
-
-    // Stop everything
-    new InstantCommand(shooterSubsystem::stop, shooterSubsystem)
-    .alongWith(new InstantCommand(indexSubsystem::stopAll, indexSubsystem))
-    .alongWith(new InstantCommand(driveSubsystem::stop, driveSubsystem)).andThen(new PrintCommand("Finished stopping everything")),
-
-    new PrintCommand("Finished IdealAuto")
-      
     );
   }
 }
